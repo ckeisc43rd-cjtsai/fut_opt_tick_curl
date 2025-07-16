3fb4b680b8eee8f1f205c913c2fe11dd54a93f77
cj.tsai <jimtsai9596@gmail.com>
2025-07-16 16:58:30 +0800

    use venv for run script

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-pip install -r requirements.txt > /dev/null 2>&1
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt > /dev/null 2>&1
 python3 crawler.py
